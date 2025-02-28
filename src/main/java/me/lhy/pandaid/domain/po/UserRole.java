586a5c75f0a56927f09e2fe6a88409118703d38c
Liu Haiyang <hilariouhiss@outlook.com>
2025-02-28 18:39:56 +0800

    feat(domain): 添加 UserRole 实体类
    - 新增 UserRole 类，用于表示用户和角色的关联关系- 包含主键 id、用户 id 和角色 id 三个字段
    - 添加了无参构造函数和有参构造函数

---
+++ src/main/java/me/lhy/pandaid/domain/po/UserRole.java
@@ -0,0 +1,27 @@
+package me.lhy.pandaid.domain.po;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRole {
+
+    @Schema(description="主键id")
+    private Long id;
+
+    @Schema(description="用户id")
+    private Long userId;
+
+    @Schema(description="角色id")
+    private Long roleId;
+
+    public UserRole(){}
+
+    public UserRole(Long userId, Long roleId) {
+        this.userId = userId;
+        this.roleId = roleId;
+    }
+}
